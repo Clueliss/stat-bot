9505671315fc6173632d4cee2f859245ff3b4f27
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2020-05-01 23:03:08 +0200

    mostly done

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,40 @@
-#[macro_use]
-extern crate lazy_static;
-
+extern crate chrono;
+#[macro_use] extern crate lazy_static;
+extern crate libc;
+extern crate serde_json;
 extern crate serenity;
 
+mod stats;
+
+
+use clap::Clap;
+
 use serenity::client::Client;
-use serenity::http::Http;
-use serenity::model::channel::Message;
+use serenity::model::channel::{Message, GuildChannel, ChannelType};
 use serenity::model::gateway::Ready;
-use serenity::model::guild::Guild;
-use serenity::model::id::{GuildId, UserId};
+use serenity::model::guild::Member;
+use serenity::model::id::{GuildId, ChannelId};
+use serenity::model::user::User;
 use serenity::model::voice::VoiceState;
 use serenity::prelude::{EventHandler, Context};
 
+use stats::Stats;
+
 use std::collections::HashMap;
-use std::time::{SystemTime, Duration};
-use std::sync::{Mutex, LockResult, MutexGuard, Arc};
+use std::fs::File;
+use std::sync::Mutex;
 
 
 lazy_static! {
-    static ref ONLINE_TIME: Mutex<HashMap<UserId, Duration>> = Mutex::new(HashMap::new());
-    static ref ONLINE_SINCE: Mutex<HashMap<UserId, SystemTime>> = Mutex::new(HashMap::new());
-}
-
-fn seconds_to_human_readable(s_total: u64) -> String {
-    let d = s_total/86400;
-    let h = (s_total - d * 86400)/3600;
-    let m = ((s_total - d * 86400) - h * 3600)/60;
-    let s = ((s_total - d * 86400) - h * 3600) - (m * 60);
-
-    format!("*{}* ***D***, *{}* ***H***, *{}* ***M***, *{}* ***S***", d, h, m, s)
+    static ref STATS: Mutex<Stats> = Mutex::new(Stats::new());
+    static ref OUTPUT_FILE_PATH: Mutex<String> = Mutex::new(String::new());
 }
 
 
-fn stat_json(ctx: &Context) -> String {
-    let ontime = ONLINE_TIME.lock().unwrap();
-    let mut buf = "{\n".to_string();
-
-    for (uid, time) in ontime.iter() {
-        buf += &format!("    \"{}\": \"{}\"\n",
-                        uid.to_user(ctx).unwrap().name,
-                        time.as_secs());
-    }
-
-    buf + "}"
-}
-
-
-fn stat_human_readable(ctx: &Context) -> String {
-    let ontime = ONLINE_TIME.lock().unwrap();
-    let mut buf = String::new();
-
-    for (uid, time) in ontime.iter() {
-        buf += &format!("{}: {}\n",
-                        uid.to_user(ctx).unwrap().name,
-                        seconds_to_human_readable(time.as_secs()));
-    }
-
-    buf
-}
-
-
-fn update_stats() {
-    let mut onsince = ONLINE_SINCE.lock().unwrap();
-    let mut ontime = ONLINE_TIME.lock().unwrap();
-
-    for (uid, timestamp) in onsince.iter_mut() {
-        let duration = SystemTime::now()
-            .duration_since(timestamp.clone())
-            .unwrap();
-
-        match ontime.get_mut(&uid) {
-            Some(t) => { *t += duration; },
-            None    => { ontime.insert(uid.clone(), duration); }
-        }
-
-        *timestamp = SystemTime::now();
-    }
+#[derive(Clap)]
+struct Opts {
+    #[clap(short = "f", long = "file")]
+    outputfile: String
 }
 
 
@@ -84,58 +43,96 @@ struct StatBot;
 impl EventHandler for StatBot {
     fn message(&self, ctx: Context, msg: Message) {
         if msg.content == ">>stats" {
-            update_stats();
+            let mut st = STATS.lock().unwrap();
+            st.update_stats();
 
             msg.channel_id
-                .send_message(&ctx, |m| m.content(stat_human_readable(&ctx)))
+                .send_message(&ctx, |m| m.content(st.as_human_readable_string(&ctx)))
                 .unwrap();
+        } else if msg.content == ">>write" {
+            let fp = OUTPUT_FILE_PATH.lock().unwrap();
+            let mut st = STATS.lock().unwrap();
+            let mut f = File::create(&*fp).unwrap();
+
+            st.flush_stats(&mut f).unwrap();
         }
     }
 
-    fn ready(&self, _ctx: Context, _rdy: Ready) {
+    fn ready(&self, ctx: Context, rdy: Ready) {
+        let mut st = STATS.lock().unwrap();
         println!("<6> now online");
+        let tlof = rdy.guilds.get(0).unwrap();
+
+        let channels: HashMap<ChannelId, GuildChannel> = tlof.id().channels(&ctx).unwrap();
+
+        for (_id, ch) in channels {
+            match ch.kind {
+                ChannelType::Voice if !ch.name.starts_with("AFK") => {
+                    let members: Vec<Member> = ch.members(&ctx).unwrap();
+
+                    for m in members {
+                        let u: User = m.user_id().to_user(&ctx).unwrap();
+
+                        if !u.bot {
+                            st.user_now_online(u.id);
+                        }
+                    }
+                },
+                _ => (),
+            }
+        }
     }
 
     fn voice_state_update(&self, ctx: Context, _: Option<GuildId>, old: Option<VoiceState>, new: VoiceState) {
-
-        let mut ontime = ONLINE_TIME.lock().unwrap();
-        let mut onsince = ONLINE_SINCE.lock().unwrap();
+        let mut st = STATS.lock().unwrap();
 
         if old.map(|o| o.channel_id) != Some(new.channel_id) {
             match new.channel_id {
                 None => {
+                    st.user_now_offline(new.user_id);
                     println!("User left: {}", new.user_id.to_user(&ctx).unwrap().name);
-
-                    if onsince.contains_key(&new.user_id) {
-                        let since = onsince.remove(&new.user_id).unwrap();
-
-                        let duration = SystemTime::now()
-                            .duration_since(since)
-                            .unwrap();
-
-                        match ontime.get_mut(&new.user_id) {
-                            Some(time) => { *time += duration; },
-                            None       => { ontime.insert(new.user_id, duration); },
-                        }
-                    }
                 },
                 Some(_) => {
+                    st.user_now_online(new.user_id);
                     println!("User joined: {}", new.user_id.to_user(ctx).unwrap().name);
-
-                    if !onsince.contains_key(&new.user_id) {
-                        onsince.insert(new.user_id, SystemTime::now());
-                    }
                 }
             }
         }
-
-        println!("onsince: {:?}", onsince);
-        println!("ontime: {:?}", ontime);
     }
 
 }
 
+
+fn signal_handler(_sig: libc::c_int) {
+    let fp = OUTPUT_FILE_PATH.lock().unwrap();
+    let mut st = STATS.lock().unwrap();
+
+    let mut f = File::create(&*fp).unwrap();
+    st.flush_stats(&mut f).unwrap();
+}
+
+
 fn main() {
+    {
+        let opts: Opts = Opts::parse();
+
+        let mut f = File::open(&opts.outputfile).unwrap();
+        let mut st = STATS.lock().unwrap();
+        let mut fp = OUTPUT_FILE_PATH.lock().unwrap();
+
+
+        *fp = opts.outputfile;
+        st.read_stats(&mut f).unwrap();
+    }
+
+    unsafe {
+        let signal_handler_fn_ptr = signal_handler as *const fn(libc::c_int);
+        let sighandler = std::mem::transmute::<*const fn(libc::c_int), libc::sighandler_t>(signal_handler_fn_ptr);
+
+        libc::signal(libc::SIGTERM, sighandler);
+        libc::signal(libc::SIGINT, sighandler);
+    }
+
     let tok = std::env::var("STAT_BOT_DISCORD_TOKEN").unwrap();
     let mut client = Client::new(tok, StatBot).unwrap();
 
---
+++ src/stats.rs
@@ -0,0 +1,116 @@
+use chrono::Utc;
+
+use serenity::model::id::UserId;
+use serenity::prelude::Context;
+
+use std::collections::BTreeMap;
+use std::io::{Read, Write};
+use std::time::{Instant, Duration};
+
+
+fn seconds_to_human_readable(s_total: u64) -> String {
+    let d = s_total/86400;
+    let h = (s_total - d * 86400)/3600;
+    let m = ((s_total - d * 86400) - h * 3600)/60;
+    let s = ((s_total - d * 86400) - h * 3600) - (m * 60);
+
+    format!("*{}* ***D***, *{}* ***H***, *{}* ***M***, *{}* ***S***", d, h, m, s)
+}
+
+fn into_json(map: BTreeMap<UserId, Duration>) -> BTreeMap<String, u64> {
+    map.into_iter()
+        .map(|(uid, dur)| (format!("{}", uid), dur.as_secs()))
+        .collect()
+}
+
+
+#[derive(Clone, Default)]
+pub struct Stats {
+    online_time: BTreeMap<UserId, Duration>,
+    online_since: BTreeMap<UserId, Instant>
+}
+
+impl Stats {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn read_stats<F: Read>(&mut self, mut f: F) -> Result<(), std::io::Error> {
+        let date = Utc::now().format("%Y-%m-%d").to_string();
+        let mut j: BTreeMap<String, BTreeMap<UserId, u64>> = serde_json::from_reader(&mut f).unwrap_or(Default::default());
+
+        let st_today = j.remove(&date).unwrap_or(Default::default());
+        self.online_time = st_today.into_iter().map(|(uid, t)| (uid, Duration::new(t, 0))).collect();
+
+        Ok(())
+    }
+
+    pub fn flush_stats<F: Write + Read>(&mut self, mut f: F) -> Result<(), std::io::Error> {
+        self.update_stats();
+
+        let mut existent: BTreeMap<String, BTreeMap<UserId, Duration>> = serde_json::from_reader(&mut f)
+            .unwrap_or(Default::default());
+
+        let date = Utc::now().format("%Y-%m-%d").to_string();
+
+        match existent.get_mut(&date) {
+            Some(st) => { *st = self.online_time.clone(); },
+            None => { existent.insert(date.clone(), self.online_time.clone()); }
+        }
+
+        let jmap: BTreeMap<String, BTreeMap<String, u64>> = existent.into_iter()
+            .map(|(s, m)| (s, into_json(m)))
+            .collect();
+
+        serde_json::to_writer(&mut f, &jmap)?;
+
+        Ok(())
+    }
+
+    pub fn update_stats(&mut self) {
+        for (uid, timestamp) in self.online_since.iter_mut() {
+            let duration = Instant::now()
+                .duration_since(timestamp.clone());
+
+            match self.online_time.get_mut(&uid) {
+                Some(t) => { *t += duration; },
+                None    => { self.online_time.insert(uid.clone(), duration); }
+            }
+
+            *timestamp = Instant::now();
+        }
+    }
+
+    pub fn as_human_readable_string(&self, ctx: &Context) -> String {
+        let mut buf = "Stats:\n".to_string();
+
+        for (uid, time) in self.online_time.iter() {
+            buf += &format!("  {}: {}\n",
+                            uid.to_user(ctx).unwrap().name,
+                            seconds_to_human_readable(time.as_secs()));
+        }
+
+        buf
+    }
+
+   pub fn user_now_offline(&mut self, uid: UserId) {
+        if self.online_since.contains_key(&uid) {
+            let since = self.online_since.remove(&uid).unwrap();
+
+            let duration = Instant::now()
+                .duration_since(since);
+
+            match self.online_time.get_mut(&uid) {
+                Some(time) => { *time += duration; },
+                None       => { self.online_time.insert(uid, duration); },
+            }
+        }
+    }
+
+    pub fn user_now_online(&mut self, uid: UserId) {
+        if !self.online_since.contains_key(&uid) {
+            self.online_since.insert(uid, Instant::now());
+        }
+    }
+
+}
